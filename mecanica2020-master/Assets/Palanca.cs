a58f4740e19ffa42da1d49ee1975ea1335144018
Koko <45299959+Domtic@users.noreply.github.com>
2020-02-19 19:39:53 -0600

    Palanca, Proeycto

---
+++ mecanica2020-master/Assets/Palanca.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Palanca : MonoBehaviour
+{
+
+    public TMPro.TMP_InputField monitaINP;
+    public TMPro.TMP_InputField atinomINP;
+    public TMPro.TMP_InputField moveUnitsINTP;
+    
+    public Rigidbody monita, atinom;
+    float MonitaKg;
+    float AtinomKg;
+    float moveUnits;
+
+
+    public void Calculate()
+    {
+        MonitaKg = float.Parse(monitaINP.text);
+        AtinomKg = float.Parse(atinomINP.text);
+        moveUnits = float.Parse(moveUnitsINTP.text);
+
+        monita.mass = MonitaKg;
+        atinom.mass = AtinomKg;
+
+        float v = (AtinomKg * moveUnits);
+        float result = v / MonitaKg;
+        Debug.Log(result);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
