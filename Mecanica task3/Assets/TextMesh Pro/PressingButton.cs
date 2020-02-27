32bcc4f49513770b8874664c682220d30278d4bf
Koko <45299959+Domtic@users.noreply.github.com>
2020-02-26 18:55:24 -0600

    TASK 3, sin rigidbody
    Anotar que pasa cuandoremuevo el rigidbody (atraviesa las paredes)

--- Mecanica task3/Assets/TextMesh Pro/PressingButton.cs
+++ Mecanica task3/Assets/TextMesh Pro/PressingButton.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PressingButton : MonoBehaviour
 {
+    float gravity = 9.8f;
+    float Weight = 1;
+    float CWeight = 1;
     float force = 0;
     bool beingPressed;
     float speed = 0.2f;
-
+    bool hitting, startGravity,moveCilinder, ballHitted, touchingGround;
     public GameObject cilinder;
+    Vector3 position;
     private void Update()
     {
+        position = transform.position;
         if(beingPressed)
         {
             transform.Translate(Vector3.up * speed);
@@ -20,8 +25,30 @@ public class PressingButton : MonoBehaviour
 
             Debug.Log(force);
         }
-            
 
+
+        if(transform.position.y > 1 && ballHitted)
+        {
+            position.y -= (gravity * Weight) * Time.deltaTime;
+            transform.position = position;
+        }
+
+
+        
+        if(moveCilinder)
+        {
+            cilinder.transform.Translate(Vector3.up *force *Time.deltaTime );
+        }
+
+        if (ballHitted)
+        {
+
+            //position.y -= (gravity * Weight) * Time.deltaTime;
+            position.x += (force * Weight) * Time.deltaTime;
+           
+            transform.position = position;
+            //transform.Translate(Vector3.right* (force / Weight) * Time.deltaTime);
+        }
     }
     public void AddEP()
     {
@@ -34,6 +61,34 @@ public class PressingButton : MonoBehaviour
 
     public void ApplyEC()
     {
+        if(!hitting)
+        {
+            hitting = true;
+            moveCilinder = true;
+            //cilinder.GetComponent<Rigidbody>().AddForce(Vector3.right * force* 200);
+        }
+        
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("golp"))
+        {
+            startGravity = true;
+            moveCilinder = false;
+            ballHitted = true;
+            //collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+        if(collision.gameObject.CompareTag("wall"))
+        {
+            ballHitted = false;
+            moveCilinder = false;
+            Debug.Log("La pared se golpeo con una fuerza de: " + collision.relativeVelocity.magnitude);
+        }
+        if(collision.gameObject.CompareTag("ground"))
+        {
+
+            touchingGround = true;
+        }
     }
 }
