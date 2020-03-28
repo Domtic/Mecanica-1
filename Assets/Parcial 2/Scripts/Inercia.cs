e1ce800d542de447da4c3b80e6db066423e54fbf
Domtic <45299959+Domtic@users.noreply.github.com>
2020-03-27 20:01:25 -0600

    Practica 2

---
+++ Assets/Parcial 2/Scripts/Inercia.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inercia : MonoBehaviour
+{
+    int forceLeft;
+    int forceRight;
+
+    public Transform limitRight,limitLeft;
+    Vector3 currentPos;
+    // Start is called before the first frame update
+    int speed;
+    bool move;
+
+    public Text fuerza1, fuerza2;
+    void Start()
+    {
+        move = false;
+        forceLeft = 0;
+        forceRight = 0;
+        currentPos = transform.position;
+    }
+
+   public void AgregarFuerza(bool side)
+    {
+        move = true;
+        if(side)
+        {
+            forceLeft += 1;
+
+        }
+        else
+        {
+            forceRight += 1;
+        }
+        fuerza1.text = "Fuerza Izquierda: " + forceRight.ToString();
+        fuerza2.text = "Fuerza derecha: " + forceLeft.ToString();
+    }
+
+    private void Update()
+    {
+
+        if(move)
+        {
+            if (forceLeft > forceRight)
+            {
+                speed = forceLeft - forceRight;
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, limitLeft.position, step);
+                if (transform.position.x == currentPos.x - forceLeft)
+                {
+                    move = false;
+                    currentPos = transform.position;
+                }
+            }
+            else if (forceRight > forceLeft)
+            {
+                speed = forceRight - forceLeft;
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, limitRight.position, step);
+                if (transform.position.x == currentPos.x + forceRight)
+                {
+                    move = false;
+                    currentPos = transform.position;
+                }
+            }
+        }
+ 
+    }
+}
